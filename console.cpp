ed7307e4cf06502d4df2f3a7468da1bb254f037e
JayE46 <jramo146@ucr.edu>
2023-04-29 17:40:10 -0700

    removed some left over code that was not needed

--- console.cpp
+++ console.cpp
@@ -12,54 +12,6 @@
 
 using namespace std;
 
-enum class CSVState {
-    UnquotedField,
-    QuotedField,
-    QuotedQuote
-};
-
-std::vector<std::string> readCSVRow(const std::string &row) {
-    CSVState state = CSVState::UnquotedField;
-    std::vector<std::string> fields {""};
-    size_t i = 0; // index of the current field
-    for (char c : row) {
-        switch (state) {
-            case CSVState::UnquotedField:
-                switch (c) {
-                    case ',': // end of field
-                              fields.push_back(""); i++;
-                              break;
-                    case '"': state = CSVState::QuotedField;
-                              break;
-                    default:  fields[i].push_back(c);
-                              break; }
-                break;
-            case CSVState::QuotedField:
-                switch (c) {
-                    case '"': state = CSVState::QuotedQuote;
-                              break;
-                    default:  fields[i].push_back(c);
-                              break; }
-                break;
-            case CSVState::QuotedQuote:
-                switch (c) {
-                    case ',': // , after closing quote
-                              fields.push_back(""); i++;
-                              state = CSVState::UnquotedField;
-                              break;
-                    case '"': // "" -> "
-                              fields[i].push_back('"');
-                              state = CSVState::QuotedField;
-                              break;
-                    default:  // end of quote
-                              state = CSVState::UnquotedField;
-                              break; }
-                break;
-        }
-    }
-    return fields;
-}
-
 //chatgpt generated sample document structure to store document in database
 struct Movie_Document {
     string poster_Link;
